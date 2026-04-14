6a517a3558dfff35816dcf8f518a631ad6020453
password <johnstamkopoulos@gmail.com>
2026-04-14 15:06:25 +0300

    Implement CICD pipeplins and GitHub Actions

--- src/app.js
+++ src/app.js
@@ -40,10 +40,14 @@ app.get('/health', (req, res) => {
 });
 
 app.get('/api', (rea, res) => {
-  res.status(200).json({ message: 'Acquisitions API is running! ' });
+  res.status(200).json({ message: 'Acquisitions API is running!' });
 });
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', usersRouters);
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route not found' });
+});
 
 export default app;
